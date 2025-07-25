711a684eb3651f56a90b416a54d0e57c138d9627
DavidKaigorodov <bossdilan2003@gmail.com>
2025-07-25 10:00:32 +0700

    Merge commit '86bcebccec31612812ae79f035a9ede63b8b98a4'

--- resources/views/components/table/Table.vue
+++ resources/views/components/table/Table.vue
@@ -8,8 +8,18 @@ defineProps({
 
 <template>
     <div class="table-container">
-        <h3 class="table-header">{{ header }}</h3>
-        <table class="table">
+        <h3 v-if="header" class="table-header">{{ header }}</h3>
+
+        <div class="table-toolbar">
+            <div class="toolbar-left">
+                <slot name="toolbar-left" />
+            </div>
+            <div class="toolbar-right">
+                <slot name="toolbar-right" />
+            </div>
+        </div>
+
+        <table class="table table-thead">
             <thead>
                 <tr>
                     <th v-for="{ key, label } in columns" :key="key">
@@ -17,18 +27,28 @@ defineProps({
                     </th>
                 </tr>
             </thead>
-            <tbody>
-                <tr v-for="(row, index) in data" :key="index">
-                    <td v-for="{ key } in columns" :key="key">
-                        <div v-if="key === 'actions'" class="table-actions">
-                            <slot name="actions" :row="row" />
-                        </div>
-                        <slot v-else :name="key" :row="row" :value="row[key]">
-                            {{ row[key] }}
-                        </slot>
-                    </td>
-                </tr>
-            </tbody>
         </table>
+
+        <div class="table-wrapper">
+            <table class="table table-tbody">
+                <tbody>
+                    <tr v-for="(row, index) in data" :key="index">
+                        <td v-for="{ key } in columns" :key="key">
+                            <div v-if="key === 'actions'" class="table-actions">
+                                <slot name="actions" :row="row" />
+                            </div>
+                            <slot
+                                v-else
+                                :name="key"
+                                :row="row"
+                                :value="row[key]"
+                            >
+                                {{ row[key] }}
+                            </slot>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
