cc557a1549df31879dfd06e8c0d7ac5f90824f60
DavidKaigorodov <bossdilan2003@gmail.com>
2025-07-25 08:53:51 +0700

    Завершил

--- resources/views/components/table/buttons/AddButton.vue
+++ resources/views/components/table/buttons/AddButton.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { Link } from "@inertiajs/vue3";
+import PlusIco from "../../icons/PlusIco.vue";
+
 defineProps({
     href: {
         type: String,
@@ -10,17 +12,6 @@ defineProps({
 
 <template>
     <Link :href="href" class="button blue-button" v-bind="$attrs">
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-plus"
-            viewBox="0 0 16 16"
-        >
-            <path
-                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"
-            />
-        </svg>
+        <PlusIco />
     </Link>
 </template>
