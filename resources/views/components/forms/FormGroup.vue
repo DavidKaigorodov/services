970ce6f98f2ac1a950147278ed777c0409d9f0be
DavidKaigorodov <bossdilan2003@gmail.com>
2025-07-29 13:15:34 +0700

    Заврешил

--- resources/views/components/forms/FormGroup.vue
+++ resources/views/components/forms/FormGroup.vue
@@ -1,9 +1,14 @@
 <script setup>
+import Label from "./Label.vue";
+
 const props = defineProps({
     name: String,
+    label: String,
 });
 </script>
+
 <template>
+    <Label :name="name" :labelText="label" />
     <div :class="['form-group', name]">
         <slot />
     </div>
