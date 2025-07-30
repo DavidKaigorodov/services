d5c11912dd9472fdcdeb608789d8c387e30cb237
DavidKaigorodov <bossdilan2003@gmail.com>
2025-07-30 09:00:37 +0700

    Merge commit '99557a770d984df5eeac36e9ac4a25521bd5faea'

---
+++ resources/views/pages/cities/create.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+
+import AuthenticatedLayout from "../../layouts/AuthenticatedLayout.vue";
+
+import HorizontalForm from "../../components/forms/HorizontalForm.vue";
+import StringInput from "../../components/forms/inputs/StringInput.vue";
+
+const form = ref({
+    name: "",
+});
+
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <HorizontalForm header="Город" sbm="Отправить">
+                    <StringInput
+                        label="Наименование"
+                        name="name"
+                        :value="form.name"
+                        @update:value="(val) => (form.name = val)"
+                        autocomplete="current-name"
+                    />
+        </HorizontalForm>
+    </AuthenticatedLayout>
+</template>
