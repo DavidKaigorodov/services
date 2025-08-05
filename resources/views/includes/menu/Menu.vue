311436900ff8e3d7a81462329f01ba3b0d1db40d
DavidKaigorodov <bossdilan2003@gmail.com>
2025-08-05 09:37:40 +0700

    dev

--- resources/views/includes/menu/Menu.vue
+++ resources/views/includes/menu/Menu.vue
@@ -3,14 +3,18 @@ import ItemMenu from "./ItemMenu.vue";
 </script>
 
 <template>
-    <nav class="sidebar">
-        <div class="sidebar-header">
-            <h3>Навигационная панель</h3>
-        </div>
+  <nav class="sidebar">
+    <div class="sidebar-header">
+      <h3>Навигационная панель</h3>
+    </div>
 
-        <ul class="sidebar-menu">
-            <ItemMenu class="sidebar-link" href="/" label="Домой" />
-            <ItemMenu class="sidebar-link" href="/" label="Домой" />
-        </ul>
-    </nav>
+    <ul class="sidebar-menu">
+      <h4>Админ панель</h4>
+      <ItemMenu class="sidebar-link" href="divisions.index" label="Подразделения" />
+      <ItemMenu class="sidebar-link" href="cities.index" label="Города" />
+      <h4>Подразделения панель</h4>
+      <ItemMenu class="sidebar-link" href="service.index" label="Услуги" />
+      <ItemMenu class="sidebar-link" href="dashboard.index" label="Календарь событий" />
+    </ul>
+  </nav>
 </template>
