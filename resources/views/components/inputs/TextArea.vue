8e1453bf104997b0cd22601d89ea27ba52a406e6
DavidKaigorodov <bossdilan2003@gmail.com>
2025-08-06 14:01:06 +0700

    Завершил

---
+++ resources/views/components/inputs/TextArea.vue
@@ -0,0 +1,47 @@
+<script setup>
+import FormItem from "../forms/FormItem.vue";
+import Label from "../forms/Label.vue";
+
+const props = defineProps({
+  rows: Number,
+  value: {
+    type: [String, Number, null],
+    default: "",
+  },
+  placeholder: {
+    type: String,
+    default: "",
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  autocomplete: {
+    type: String,
+    default: "",
+  },
+  id: String,
+  name: String,
+  label: String,
+});
+
+const emit = defineEmits(["click"]);
+const inputId = props.id ?? props.name;
+const placeholderText = props.placeholder ?? "";
+</script>
+<template>
+  <FormItem :name="name">
+    <Label :labelText="label" />
+    <textarea
+      :rows="rows"
+      :id="inputId"
+      :name="name"
+      :value="value ?? ''"
+      @input="$emit('update:value', $event.target.value)"
+      :placeholder="placeholderText"
+      :disabled="disabled"
+      @click="$emit('click', $event)"
+      v-bind="$attrs"
+    />
+  </FormItem>
+</template>
