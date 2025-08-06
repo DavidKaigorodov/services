6462fa3879487c43f43297f6e038767782c40b7e
DavidKaigorodov <bossdilan2003@gmail.com>
2025-08-06 15:21:16 +0700

    Merge commit '6d8fb38dc809dba7f147a28372eda98f00f276b5'

--- resources/views/components/forms/FormGroup.vue
+++ resources/views/components/forms/FormGroup.vue
@@ -2,16 +2,16 @@
 import Label from "./Label.vue";
 
 const props = defineProps({
-    name: String,
-    label: String,
+  name: String,
+  label: String,
 });
 </script>
 
 <template>
-    <div class="form-group-container">
-        <Label name="group" :labelText="label"/>
-        <div :class="['form-group', name]">
-            <slot />
-        </div>
+  <div class="form-group-container">
+    <Label name="group" :labelText="label" />
+    <div :class="['form-group', name]">
+      <slot />
     </div>
+  </div>
 </template>
