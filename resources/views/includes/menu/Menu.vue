e2d0283414bfdb927106ce2c0ce885539b366b9b
DavidKaigorodov <bossdilan2003@gmail.com>
2025-08-07 09:58:14 +0700

    dev

--- resources/views/includes/menu/Menu.vue
+++ resources/views/includes/menu/Menu.vue
@@ -14,6 +14,7 @@ import ItemMenu from "./ItemMenu.vue";
       <ItemMenu class="sidebar-link" href="cities.index" label="Города" />
       <h4>Подразделения панель</h4>
       <ItemMenu class="sidebar-link" href="service.index" label="Услуги" />
+      <ItemMenu class="sidebar-link" href="subscribes.index" label="О нас" />
       <ItemMenu class="sidebar-link" href="dashboard.index" label="Календарь событий" />
     </ul>
   </nav>
