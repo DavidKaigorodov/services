bb55d0c7af960bfcae91871ed5d9da8b53d41948
DavidKaigorodov <bossdilan2003@gmail.com>
2025-08-14 15:47:38 +0700

    Merge commit 'aaf2aaf5ee03c798f469d8298e2344ae69fb7400'

---
+++ resources/views/components/buttons/table-buttons/GoToButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+import { GoToIco } from "@components";
+
+defineProps({
+  href: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <Link :href="href" class="button blue-button" v-bind="$attrs">
+    <GoToIco />
+  </Link>
+</template>
